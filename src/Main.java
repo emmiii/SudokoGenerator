c23b6d0826312008b5b7820edc68fd728079973b
Emma Frisk <frisk.emma@hotmail.com>
2024-09-28 22:20:17 +0200

    show unsolved sudoko first, then ask user if they wish to see the answer before showing it

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -8,14 +9,40 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Creating sudoko...");
-        var matrix = new Matrix(9);
+        var size = 9;
+        var matrix = new Matrix(size);
 
-        //print the matrix
-        for(int column = 0; column < matrix.GetSize(); column++) {
-            for(int row = 0; row < matrix.GetSize(); row++) {
-                System.out.print("["+matrix.GetMatrix()[column][row]+"]");
+        //print partial matrix to allow user to solve it
+        for(int column = 0; column < size; column++) {
+            for(int row = 0; row < size; row++) {
+                //show roughly 1/4 of the numbers
+                var showNumber = new Random().nextInt(4) == 0;
+                if(showNumber) {
+                    System.out.print("["+matrix.GetMatrix()[column][row]+"]");
+                }
+                else {
+                    System.out.print("[ ]");
+                }
+
             }
             System.out.println();
+        }
+        // Scanner definition
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Would you like to see the answer?");
+        String showAnswer = scn.next();
+        //take input from user
+        System.out.println();
+
+        if(showAnswer.equalsIgnoreCase("yes")) {
+            //print the full matrix
+            for(int column = 0; column < size; column++) {
+                for(int row = 0; row < size; row++) {
+                    System.out.print("["+matrix.GetMatrix()[column][row]+"]");
+                }
+                System.out.println();
+            }
         }
     }
 }
